d5869d616a0c3b3b3ca43584bc3cb95ecfc94caa
leechun04 <pangleechun2016@gmail.com>
2025-12-09 15:16:34 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cho44awksuyv.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "password123"
+customdb = "employee"
+custombucket = "pangleechun-employee"
+customregion = "us-east-1"
+
